368e9dc34e5fda1edc02e6db95407fd7a7f024c6
Stephen Brundage <stephen.brundage12@gmail.com>
2024-11-11 07:57:00 -0500

    Initial Commit

---
+++ FlickrPhoto/FlickrPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrPhotoApp.swift
+//  FlickrPhoto
+//
+//  Created by Stephen Brundage on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
